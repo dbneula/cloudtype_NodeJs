e2e7f32046f527bc662f3bf8505b3b9ce0c76523
DESKTOP-949N17S\JAM_3080 <neula@naver.com>
2023-07-27 20:24:17 +0900

    /webar

--- src/server.js
+++ src/server.js
@@ -7,6 +7,9 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views/");
 
 app.get("/", (req, res) => res.render("home"));
+app.get("/webar", (req, res) => {
+    res.sendFile(__dirname + "/views/webar.html");
+})
 
 const handleListen = ()=>console.log('Listening on http://localhost:3000');
 app.listen(3000, handleListen);
